d6a0825ac65177688962349017f78290b9eb06ec
Sunil Prakash <prakashsunil@gmail.com>
2015-05-17 14:20:53 +0800

    added

--- shasha/settings.py
+++ shasha/settings.py
@@ -102,6 +102,8 @@ DATABASES['default'] =  dj_database_url.config()
 
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
+
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 

2cc605f4b507407d791441ea4eb79166ab4b54d2
Sunil Prakash <sunilprakash@SunilsiMac.singnet.com.sg>
2015-06-06 07:59:38 +0800

    added angular support:

--- shasha/settings.py
+++ shasha/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'register',
 )
 
@@ -121,3 +122,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
